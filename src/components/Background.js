55877d9368b90c184b14e4ea4a92583f228e6e46
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-03-24 10:53:28 +0530

    added background

---
+++ src/components/Background.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Leaf from "../../public/videos/leavs.mp4"
+const Background = () => {
+  return (
+    <video className="bg-video" autoPlay muted loop>
+      <source src={Leaf} type="video/mp4" />
+    </video>
+  )
+}
+
+export default Background
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import Background from "../components/Background"
 
+import "../../public/index.css"
+import Header from "../components/header"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Background />
+      <Header headerText="Hi I'm Ankit Kumar Gupta" />
+    </div>
+  )
 }
